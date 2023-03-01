4252c544de12e08b24f67b9bb565cde4e6107c63
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2023-03-01 21:10:32 +0100

    initial variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <Arduino.h>
 
+long currentLightValue = 0;
+long startLightValue = 0;
+unsigned long startTime;
+unsigned long passedTime;
+unsigned long minutes;
+unsigned long seconds;
+unsigned long tenths;
+
+bool running = false;
+
+int pinStartReset = 2;
+int button = LOW;
+
+int pinLEDGreen = 3;
+int pinLEDYellow = 4;
+int pinLEDRed = 5;
+
+int pinLight = A0;
+
 void setup() {
-  // put your setup code here, to run once:
+	pinMode(pinStartReset, INPUT);
+	pinMode(pinLEDGreen, OUTPUT);
+	pinMode(pinLEDYellow, OUTPUT);
+	pinMode(pinLEDRed, OUTPUT);
+	Serial.begin(9600); // remove after tests
 }
 
 void loop() {

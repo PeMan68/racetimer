6371b3fa7eb6f990c8b6e1bef329713ad868b1c2
PeMan68 <35003927+PerManholm@users.noreply.github.com>
2023-03-01 21:48:46 +0100

    trafficlight sequences

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,11 @@ int pinLEDRed = 5;
 
 int pinLight = A0;
 
+void rollingSequence();
+void startSequence();
+void redToGreenSequence();
+void greenToRedSequence();
+
 void setup() {
 	pinMode(pinStartReset, INPUT);
 	pinMode(pinLEDGreen, OUTPUT);
@@ -27,6 +32,78 @@ void setup() {
 	Serial.begin(9600); // remove after tests
 }
 
+
+void startSequence()
+{
+	// rollingSequence();
+	for (int i = 0; i < 5; i++)
+	{
+		digitalWrite(pinLEDGreen, HIGH);
+		digitalWrite(pinLEDYellow, HIGH);
+		digitalWrite(pinLEDRed, HIGH);
+		delay(500 - i * 100);
+		digitalWrite(pinLEDGreen, LOW);
+		digitalWrite(pinLEDYellow, LOW);
+		digitalWrite(pinLEDRed, LOW);
+		delay(500 - i * 100);
+	}
+		redToGreenSequence();
+		delay(2000);
+		greenToRedSequence();
+
+}
+
+void redToGreenSequence()
+{
+		digitalWrite(pinLEDRed, HIGH);
+		digitalWrite(pinLEDGreen, LOW);
+		digitalWrite(pinLEDYellow, LOW);
+		delay(2000);
+		digitalWrite(pinLEDYellow, HIGH);
+		delay(2000);
+		digitalWrite(pinLEDGreen, HIGH);
+		digitalWrite(pinLEDRed, LOW);
+		digitalWrite(pinLEDYellow, LOW);
+}
+
+void greenToRedSequence()
+{
+		digitalWrite(pinLEDGreen, HIGH);
+		digitalWrite(pinLEDRed, LOW);
+		digitalWrite(pinLEDYellow, LOW);
+		delay(2000);
+		digitalWrite(pinLEDGreen, LOW);
+		digitalWrite(pinLEDYellow, HIGH);
+		delay(2000);
+		digitalWrite(pinLEDRed, HIGH);
+		digitalWrite(pinLEDYellow, LOW);
+}
+
+void rollingSequence()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		digitalWrite(pinLEDGreen, HIGH);
+		delay(100);
+		digitalWrite(pinLEDYellow, HIGH);
+		delay(100);
+		digitalWrite(pinLEDRed, HIGH);
+		delay(100);
+		digitalWrite(pinLEDGreen, LOW);
+		delay(100);
+		digitalWrite(pinLEDYellow, LOW);
+		delay(100);
+		digitalWrite(pinLEDRed, LOW);
+		delay(100);
+	}
+
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
+	button = digitalRead(pinStartReset);
+	if (button == HIGH)
+	{
+		startSequence();
+	}
+	
 }
